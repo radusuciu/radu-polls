3d12e77e3a934264d180f001cef912c7877fde57
Radu Suciu <radusuciu@gmail.com>
2021-05-10 11:57:41 -0700

    Remove old method

--- polls/views.py
+++ polls/views.py
@@ -18,9 +18,6 @@ def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}.")
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
-
-def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
